661a8e596b8869374c9886b5bd837e0a82355586
Raja Naveed <rajadahri11@gmail.com>
2023-09-12 01:37:46 +0500

    card

---
+++ app/page.js
@@ -0,0 +1,13 @@
+'use client';
+import Card from "@/components/Card";
+
+export default function Home() {
+
+  return (
+    <div className="h-screen flex flex-wrap gap-x-2">
+     <Card/>
+     <Card/>
+
+    </div>
+  );
+}
